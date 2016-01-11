f996a35b83f9d56edc344c035d6fff3107b1156d
DenzylFontaine <ddenzfong@gmail.com>
2016-01-10 21:42:39 -0700

    Trying intro concepts and learning deployments

---
+++ intro.php
@@ -0,0 +1,17 @@
+<?php // true false test
+$myname = "denz";
+$myage = "21";
+
+echo "a: " . 73 . "<br>";
+echo "b: " . "HELLO" . "<br>";
+echo "c: " . FALSE . "<br>";
+echo "d: " . $myname . "<br>";
+echo "e: " . $myage . "<br>";
+?>
+
+
+<?php // repeating String functions
+echo strrev(" .dlrow olleH"); // reverse string
+echo str_repeat("Hip ", 2); // repeat string
+echo strtoupper("hooray! "); // string to uppercase
+?>
